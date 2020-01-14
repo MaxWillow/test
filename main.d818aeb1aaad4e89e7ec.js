98db3d9eda70e980a0f6cb711bf99328281d8958
Max Verba <maxwillow93@gmail.com>
2020-01-14 23:41:09 +0200

    Updates

---
+++ main.d818aeb1aaad4e89e7ec.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{"7hx4":function(n,e,o){"use strict";o.r(e);o("Muwe"),o("y89P");var c=document.querySelector(".js-btn");console.log(c),c.addEventListener("click",(function(){window.location.replace("./second.html")}))}},[["7hx4",1,2]]]);
+//# sourceMappingURL=main.d818aeb1aaad4e89e7ec.js.map
